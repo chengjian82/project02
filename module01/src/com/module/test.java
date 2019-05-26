42995eceb92c855dd7cadcafd1fddcb88ac5466a
jian.cheng <645814936@qq.com>
2019-05-26 16:00:51 +0800

    项目

---
+++ module01/src/com/module/test.java
@@ -0,0 +1,7 @@
+package com.module;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("test.main");
+    }
+}
---
+++ src/com/jian/test.java
@@ -0,0 +1,7 @@
+package com.jian;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("test.main");
+    }
+}
